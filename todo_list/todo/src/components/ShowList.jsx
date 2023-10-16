a3e9746843c2509d1b7de05371d4bbc06d06faff
choikeunyoung <ought119@gmail.com>
2023-10-16 20:10:08 +0900

    삭제 만드는중

--- todo_list/todo/src/components/ShowList.jsx
+++ todo_list/todo/src/components/ShowList.jsx
@@ -3,23 +3,31 @@ import styles from "../styles/show.module.css"
 import edit from "../assets/edit.png"
 import Delete from "../assets/delete.png"
 
+const changeStyle = (event) => {
+    if (event.target.classList.contains(styles[`show-list__active`])) {
+        event.target.classList.remove(styles[`show-list__active`]);
+    }
+    else {
+        event.target.classList.add(styles[`show-list__active`]);
+    }
+}
+
 
 const ShowList = (probs) => {
     const list_num = probs.showlist;
     const listitems = list_num.map((nums) => 
         <div className={styles[`show-list`]}>
-            <span>{ nums.id }.</span>
-            <li key={nums.id} className={styles[`show-list__items`]} id=nums.id}">{nums.todo}</li>
+            <li key={nums.id} className={styles[`show-list__items`]} onClick={changeStyle}>{nums.todo}</li>
             <div className={styles[`show-list__imagebox`]}>
                 <img src={edit} value={nums.id} alt=""/>
-                <img src={Delete} value={nums.id} alt=""/>
+                <img src={Delete} value={nums.id} alt="" onClick={probs.removelist}/>
             </div>
         </div>
         )
     return (
-        <>
-        {listitems}
-        </>
+        <ul>
+            {listitems}
+        </ul>
     )
 }
 
--- todo_list/todo/src/pages/TodoList.jsx
+++ todo_list/todo/src/pages/TodoList.jsx
@@ -4,10 +4,10 @@ import ShowList from "../components/ShowList";
 import { useState } from "react";
 import styles from "../styles/todo.module.css"
 
+
 const TodoList = () => {
     const [lis_todos, setListTodos] = useState([]);
     const [nextId,setNextId] = useState(1)
-
     const todo_func = (num_list) => {
         const setting_list = [...lis_todos];
         setting_list.push(num_list);
@@ -19,7 +19,7 @@ const TodoList = () => {
         <article className={styles[`content-box`]}>
             <section className={styles[`content-box__todo-list`]}>
                 <h1>Todo List</h1>
-                <ShowList showlist={lis_todos} />
+                <ShowList showlist={lis_todos}/>
                 <CreateList getData={todo_func} nextId={nextId}/>
             </section>  
         </article>
